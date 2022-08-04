f72235137f5c2db33e054be60c01462dd5d94846
agnekauk <agnenemeikaite@gmail.com>
2022-08-04 14:40:40 +0300

    JS update

--- main.js
+++ main.js
@@ -1,45 +1,57 @@
 let img = document.querySelector("img");
 
+function reset() {
+    let div = document.querySelector(".div");
+    let pirmasP = div.firstChild;
+    img.style.float = "none";
+    img.style.marginLeft = "" + (document.querySelector("main").offsetWidth - 267) / 2 + "px";
+    img.style.display = "block";
+    div.insertBefore(img, pirmasP);
+}
+
+
 let leftBtn = document.querySelector(".left");
 
-leftBtn.addEventListener("click", reset);
 leftBtn.addEventListener("click", function () {
-    img.style.margin = "0px 10px";
-    img.style.float = "left";
+    reset();
+    if (window.innerWidth < 391) {
+        alert("Jūsų ekranas per siauras šiai funkcijai išabandyti");
+    } else {
+        img.style.margin = "0px 10px";
+        img.style.float = "left";
+        console.log(img);
+    }
 })
 
 let rightBtn = document.querySelector(".right");
 
-rightBtn.addEventListener("click", reset);
 rightBtn.addEventListener("click", function () {
-    img.style.float = "right";
-    img.style.margin = "0px 10px";
+    reset();
+    if (window.innerWidth < 391) {
+        alert("Jūsų ekranas per siauras šiai funkcijai išabandyti");
+    } else {
+        img.style.float = "right";
+        img.style.margin = "0px 10px";
+        console.log(img);
+    }
 })
 
 let hideBtn = document.querySelector(".hide");
 
-hideBtn.addEventListener("click", reset);
 hideBtn.addEventListener("click", function () {
+    reset();
     img.style.display = "none";
 })
 
 
 let flipBtn = document.querySelector(".flip");
 
-flipBtn.addEventListener("click", reset);
 flipBtn.addEventListener("click", function () {
+    reset();
     let main = document.querySelector("main");
     main.appendChild(img);
 })
 
-function reset() {
-    let div = document.querySelector(".div");
-    let pirmasP = div.firstChild;
-    div.insertBefore(img, pirmasP);
-    img.style.float = "none";
-    img.style.marginLeft = "calc((800px - 267px) /2)";
-    img.style.display = "block";
-}
 
 let original = document.querySelector(".back");
 original.addEventListener("click", reset);
